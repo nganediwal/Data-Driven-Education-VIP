6b5ae151d1e33d637dfd01af6c7253224170bcf6
Manley Roberts <manleyroberts@gatech.edu>
2019-02-17 17:05:44 -0500

    adding general use queries page

---
+++ sql/general-queries.sql
@@ -0,0 +1,4 @@
+--General queries for use when cleaning SQL data for ISyE 6501 Fall 2018.
+
+--selecting the course itself
+select * from edx.courses where course_id='course-v1:GTx+ISYE6501x+3T2018';
