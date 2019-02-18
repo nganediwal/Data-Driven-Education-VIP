89012ea7cd332347fa200801ce6a4759408f65b3
Yili Hui <eli970616@lawn-128-61-31-230.lawn.gatech.edu>
2019-02-18 14:14:48 -0500

    update the general-queries.sql and add one query for inner join to get all verified students grade

--- sql/general-queries.sql
+++ sql/general-queries.sql
@@ -18,3 +18,17 @@ select user_id, mode, is_active, date_joined, created, last_login
 		on isyestudents.id=edx.student_courseenrollment.user_id and isyestudents.course_id=edx.student_courseenrollment.course_id) 
 		as isyestudentcombo
 	where mode='verified';
+
+-- inner join on course_id and user_id to get all verified students grades
+select isyestudentcombo.user_id, mode, is_active, date_joined, isyestudentcombo.created, last_login,edx.grades_persistentcoursegrade.percent_grade
+	from (
+		(select id, last_login, course_id, date_joined 
+			from edx.auth_user_censored 
+			where course_id='course-v1:GTx+ISYE6501x+3T2018' 
+			and is_staff=false) 
+			as isyestudents 
+		left join edx.student_courseenrollment 
+		on isyestudents.id=edx.student_courseenrollment.user_id and isyestudents.course_id=edx.student_courseenrollment.course_id) 
+		as isyestudentcombo 
+	full outer join  edx.grades_persistentcoursegrade on  edx.grades_persistentcoursegrade.user_id = isyestudentcombo.user_id and edx.grades_persistentcoursegrade.course_id ='course-v1:GTx+ISYE6501x+3T2018' 
+	where mode='verified' and  edx.grades_persistentcoursegrade.percent_grade > 0.0;
