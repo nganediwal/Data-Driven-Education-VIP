086e11a64bc5a4b3d2b4ab665118efbca08cb1f8
Shawn Staudaher <sstaudaher3@hindley.cc.gatech.edu>
2019-03-11 11:34:34 -0400

    Edited df.sql and minor cleanup

--- sql/df.sql
+++ sql/df.sql
@@ -1,4 +1,6 @@
 -- The dataframe used to train our models. Based on Gardner+Brooks2018
+DROP TABLE IF EXISTS edx.dataframe CASCADE;
+
 CREATE TABLE edx.dataframe (
     id VARCHAR(255) PRIMARY KEY,
     course_id VARCHAR(255),
@@ -8,7 +10,7 @@ CREATE TABLE edx.dataframe (
     clickstream_active_days SMALLINT,
     clickstream_quiz_views INT,
     clickstream_exam_views INT,
-    quiz_peer_views INT,
+    clickstream_quiz_peer_views INT,
     quiz_lead_time INT,
     quiz_raw_points INT,
     quiz_avg_score NUMERIC,
@@ -28,8 +30,8 @@ CREATE TABLE edx.dataframe (
     forum_sentiment_delta NUMERIC,
     forum_n_threads INT,
     forum_n_words_bigrams INT,
-    forum_f_reading_ease NUMERIC,
-    forum_fk_readling_level NUMERIC,
+    forum_f_reading_ease INT,
+    forum_fk_readling_level INT,
     forum_net_votes INT
 );
 
@@ -38,13 +40,13 @@ COMMENT ON COLUMN edx.dataframe.clickstream_forum_views IS 'Number of pageviews 
 COMMENT ON COLUMN edx.dataframe.clickstream_active_days IS 'Number of days for which user registered any clickstream activity (maximum of 7)';
 COMMENT ON COLUMN edx.dataframe.clickstream_quiz_views IS 'Number of pageviews of quiz attempt pages, as measured by clickstream features';
 COMMENT ON COLUMN edx.dataframe.clickstream_exam_views IS 'Number of pageviews of exam-type quiz pages, as measured by clickstream features';
-COMMENT ON COLUMN edx.dataframe.quiz_peer_views IS 'Number  of  pageviews  of  human-graded  quiz  pages,  as  measured  byclickstream features';
-COMMENT ON COLUMN edx.dataframe.quiz_lead_time IS 'Time between a quiz submission and deadline for all submissions; discretizedbuckets for t≥7 days (2), 3≤t<7 (1), 1≤t<3, 0≤t<1 (0), and late (-1)';
+COMMENT ON COLUMN edx.dataframe.clickstream_quiz_peer_views IS 'Number  of  pageviews  of  human-graded  quiz  pages,  as  measured  by clickstream features';
+COMMENT ON COLUMN edx.dataframe.quiz_lead_time IS 'Time between a quiz submission and deadline for all submissions; discretized buckets for t≥7 days (2), 3≤t<7 (1), 1≤t<3, 0≤t<1 (0), and late (-1)';
 COMMENT ON COLUMN edx.dataframe.quiz_raw_points IS 'Sum of total raw points earned on quizzes';
 COMMENT ON COLUMN edx.dataframe.quiz_avg_score IS 'Average raw score on all assignments';
 COMMENT ON COLUMN edx.dataframe.quiz_raw_points_per_submission IS 'Total raw points divided by total submissions';
 COMMENT ON COLUMN edx.dataframe.quiz_n_submissions IS 'Total count of quiz submissions';
-COMMENT ON COLUMN edx.dataframe.quiz_percent_allowed_submissions IS 'Total count of quiz submissions as a percent of the maximum allowedsubmissions';
+COMMENT ON COLUMN edx.dataframe.quiz_percent_allowed_submissions IS 'Total count of quiz submissions as a percent of the maximum allowed submissions';
 COMMENT ON COLUMN edx.dataframe.quiz_percent_max_submissions IS 'A student total number of quiz submissions as a percent of themaximum number of submissions made by any student in the course';
 COMMENT ON COLUMN edx.dataframe.quiz_percent_corrent_submissions IS 'Percentage of the total submissions that were correct';
 COMMENT ON COLUMN edx.dataframe.quiz_weekly_delta IS 'Difference between current week average and previous week average quizgrade';
@@ -61,5 +63,4 @@ COMMENT ON COLUMN edx.dataframe.forum_n_words_bigrams IS 'Count of unique words/
 COMMENT ON COLUMN edx.dataframe.forum_f_reading_ease IS 'Flesch Reading Ease score, discretized into separate features in increments of 10 from0 to 100 (Kincaid et al., 1975)';
 COMMENT ON COLUMN edx.dataframe.forum_fk_readling_level IS 'Flesch-Kincaid grade level, discretized into separate features in increments of1 from 0 to 20 (Kincaid et al., 1975)';
 COMMENT ON COLUMN edx.dataframe.forum_net_votes IS 'Total net upvotes users’ posts received (positive – negative)';
-
 GRANT SELECT ON edx.dataframe TO edx_student;
