9b5def16bc6101bd15d8a10796021e12f12aa38e
Manley Roberts <manleyroberts@gatech.edu>
2019-04-01 04:12:08 -0400

    added output-to-csv functionality to attributes.sh

--- mongodb/attributes.py
+++ mongodb/attributes.py
@@ -1,20 +1,39 @@
 import sys
 import json
+import numpy as np
 
 json_file = sys.argv[1]
 
 with open(json_file) as input_json:
 	input_json_string = input_json.read()
-# with open(sys.argv[2], 'w') as output_file:
-# 	output_file.write(input_json_string)
 data = json.loads(input_json_string)
 
 events = data["events"]
-output = []
+id_list = []
+fields = []
+
 for event in events:
-	# if event["_id"] in [x[0] for x in output]:
+	if event["_id"] not in id_list:
+		id_list.append(event["_id"])
+	if event["event"] not in fields:
+		fields.append(event["event"])
 
-	# else:
-	# 	output += [[event["_id"], event]]
+output = {}
 
-	# TODO: build dataframe from json
+id_list = sorted(id_list)
+
+for this_id in id_list:
+	this_tuple = []
+	for f in fields:
+		this_tuple.append(0)
+	output.update({this_id: this_tuple})
+
+for event in events:
+	this_tuple = output[event["_id"]]
+	this_tuple[fields.index(event["event"])] = event["count"]
+	output.update({this_id: this_tuple})
+
+# dtype = dict(names = fields, formats=formats)
+output_array = np.array([[x[0]] + x[1] for x in list(output.items())])
+
+np.savetxt(sys.argv[2], output_array, delimiter=",")
