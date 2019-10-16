5f7d4f3eae0f09a069294ed52858d5f109bb1f94
Hosuk Choi <hosukisawesome@hotmail.com>
2019-10-16 12:03:30 -0400

    querying for hyperlink clicks

--- mongo_tools/queries.py
+++ mongo_tools/queries.py
@@ -8,34 +8,35 @@ server and run a simple analysis. See README.md for instructions.
 This is an additional example that shows how to swap in your own custom query.
 """
 
-element = "edx_forum_thread_viewed"
-
+#element = "page_close"
+element = "edx_ui_lms_link_clicked"
 pipeline =  [
                 { "$match": 
-                    {"event_type": element}
+                    {"event_type": "edx.ui.lms.link_clicked"}
                 },
                 { "$group": { "_id": "$context.user_id", element: {"$sum": 1}}},
                 { "$sort": {"total": -1 } }
             ]
 
-# Insert your query here.
+# getting all event types
 def get_cursor_event_types(db):
     query = db.ISYE6501.find().distinct('event_type') #getting all event types
     return query
 
+#getting pipelined data
 def get_cursor(db):
-    query = db.ISYE6501.find()
-    #query = db.ISYE6501.aggregate(pipeline)
+    query = db.ISYE6501.aggregate(pipeline)
     return query
 
 def get_dataframe(db, cursor):
     """Returns a dataframe of the results of the query."""
-    #print(list(cursor))
+    print(list(cursor))
     return pd.DataFrame(list(cursor))
 
 def main():
     db = connect.get_db('clickstream')
-    cursor = get_cursor_event_types(db)
+    cursor = get_cursor(db)
+ #   cursor = get_cursor_event_types(db)
     dataframe = get_dataframe(db, cursor)
     print(dataframe)
 if __name__ == '__main__':
