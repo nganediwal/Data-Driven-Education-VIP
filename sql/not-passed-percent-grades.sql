349b82c4c4fa17d2c73ee767d08d91711fa64533
Prasad, Anusha <aprasad72@gatech.edu>
2019-11-08 15:26:36 -0500

    Delete not-passed-percent-grades.sql

--- sql/not-passed-percent-grades.sql
+++
@@ -1,6 +0,0 @@
-select course_id, user_id, percent_grade, letter_grade, passed_timestamp
-from edx.grades_persistentcoursegrade
-/* < 0.7 : not pass 
-   >= 0.7 : pass */
-where percent_grade < 0.7
-limit 500
