7ba11e16ce5f330462dd03cc3ffb7161b096476d
Zhao, Huimin <hzhao326@gatech.edu>
2020-10-16 14:21:35 -0400

    Update model_pipeline.py

--- fall2020_model/model_pipeline.py
+++ fall2020_model/model_pipeline.py
@@ -15,6 +15,10 @@ from sklearn.metrics import mean_squared_error
 from collections import Counter, OrderedDict
 from ast import literal_eval
 from itertools import chain
+import matplotlib.pyplot as plt
+from pandas import read_excel
+from scipy import stats
+
 
 output_variable = "percent_progress"
 
@@ -65,48 +69,7 @@ def plot_feature_combo(data, column1, column2, style):
     data.plot(x=column1, y=column2, kind=style)
     plt.savefig('./plots/' +column1 +'_'+ column2 + '.png') 
 
-def clean_data(df):
-    del df['English']
-    del df['US']
-    df["level_of_education"]=df["level_of_education"].fillna("Not Specified")
 
-
-    # Temp drop all na value
-    df = df.dropna(axis=0)
-
-    #plot indivisual variable to obsere outlier
-    sns_plot=sns.boxplot(df["year_of_birth"])
-    sns_plot.figure.savefig("./plots/yob_before.png")
-
-    # Create z-score columns of all numerical variables
-    df2 = df
-
-    z_column = list(df.columns)
-    z_column .remove('gender')
-    z_column .remove('level_of_education')
-    print(df2[z_column])
-
-    for col in z_column:
-        col_zscore = col + '_zscore'
-        df2[col_zscore] = (df2[col] - df2[col].mean())/df2[col].std(ddof=0)
-    
-    #drop rows with z-score higher than 3 or lower than -3
-
-    for col in z_column:
-        df2.drop(df2[df2[col] < -3].index, inplace = True)
-        df2.drop(df2[df2[col] > 3].index, inplace = True) 
-
-
-    #plot numerical variable to see if we seccefully removed outliers
-    sns_plot=sns.boxplot(df2["year_of_birth"])
-    sns_plot.figure.savefig("./plots/yob_after.png")
-
-    #create dummy for gender
-
-    df["gender"]=df["gender"].fillna("Not Specified")
-    dummiesgender = pd.get_dummies(df['gender']).rename(columns=lambda x: 'gender_' + str(x))
-    df = pd.concat([df, dummiesgender], axis=1)
-    return df
 
 def clean_data_null(df):
     print(df.isnull().sum().sort_values(ascending=False))
@@ -153,7 +116,52 @@ def clean_data_null(df):
 
     return df;
 
+def clean_data_outlier(df):
+	      
+   df = df.set_index('percent_progress')
+   del df['English']
+   del df['US']
+   #del df['Age']
+   df["level_of_education"]=df["level_of_education"].fillna("Not Specified")
+   # Temp drop all na value
+   df = df.dropna(axis=0)
+   #plot indivisual variable to obsere outlier
+   sns.boxplot(df["year_of_birth"])
+   #This method only remove outlier for numerical independent variable
+   del df ["level_of_education"]
+   del df ["gender"]
 
+   #remove outlier based on z-score
+   #remove outlier
+   z_scores = stats.zscore(df)
+   abs_z_scores = np.abs(z_scores)
+   filtered_entries = (abs_z_scores < 2).all(axis=1)
+   filtered_df = df[filtered_entries]
+   #plot again to see if it works
+   sns.boxplot(filtered_df["year_of_birth"])
+   #create outlier table for outlier analysis
+   for col in df:
+       col_zscore = col + '_zscore'
+       df[col_zscore] = (abs((df[col] - df[col].mean())/df[col].std(ddof=0))>2)*1
+   z_cols = [col for col in df.columns if 'zscore' in col]
+   df_outlier= pd.DataFrame() 
+   for col in z_cols:
+       df_outlier[col]= df[col]
+   #for the non-numerical factors, My plan is to create dummy variables, run the regression model 
+   #and plot cook's distance to observe and remove outlier >1 but I don't know how to do it in Python... 
+   #So I worked in R, I would keep learning how to do it in python and update this script later this weekend
+   #for now,I would post the plots of numerical factors in the plot folder
+
+   #create dummy for gender
+   #df["gender"]=df["gender"].fillna("Not Specified")
+   #dummiesgender = pd.get_dummies(df['gender']).rename(columns=lambda x: 'gender_' + str(x))
+   #df = pd.concat([df, dummiesgender], axis=1)
+   #df = df.drop(['gender'], inplace=True, axis=1)
+   return filtered_df
+
+
+
+	      
 def feature_explortion(data):
     '''
     Explore features and return a list of features thats needs to be included in the model
