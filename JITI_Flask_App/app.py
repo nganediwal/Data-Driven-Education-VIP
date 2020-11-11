d5b9add5a975a7e23b33d0597018ff8575f7994a
Hosuk Choi <hchoi346@gatech.edu>
2020-11-10 19:37:08 -0500

    fixing scrolling horizontally

--- JITI_Flask_App/app.py
+++ JITI_Flask_App/app.py
@@ -54,12 +54,14 @@ SIDEBAR_STYLE = {
     "width": "16rem",
     "padding": "2rem 1rem",
     "background-color": "#f8f9fa",
+    "z-index" : '5',
 }
 
 CONTENT_STYLE = {
     "margin-left": "18rem",
     "margin-right": "2rem",
     "padding": "2rem 1rem",
+    "z-index" : '2',
 }
 
 sidebar = html.Div(
